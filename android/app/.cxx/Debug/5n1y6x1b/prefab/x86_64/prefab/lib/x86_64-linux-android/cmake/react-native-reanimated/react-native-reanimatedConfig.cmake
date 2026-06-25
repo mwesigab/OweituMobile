a4337eb5a97c7817b07b040b4aad45841f5f234f
joshua <joshuaaijuka10@gmail.com>
2026-06-25 10:23:24 +0300

    changes

--- android/app/.cxx/Debug/5n1y6x1b/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
-set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "/home/joshua/Desktop/Oweitu Mobile App/OweituMobile/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/q291m5c2/obj/x86_64/libreanimated.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/joshua/Desktop/Oweitu Mobile App/OweituMobile/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
